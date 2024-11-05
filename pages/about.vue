05660ecaf91547b8a34456e3bf117b506c94e307
Ramazan <romagt2015@mail.ru>
2024-11-05 15:33:54 +0300

    Nuxt

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <header class="about">
+        <h1 class="header__about">peta</h1>
+    </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <header class="header">
+        <h1 class="header__title">Nuxt Minimal Starter</h1>
+    </header>
+</template>
